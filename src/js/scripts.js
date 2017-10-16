27e680e4d49f205db62ffb699add8a5fd5c6dfd8
Oksana <merkulova.0032@gmail.com>
2017-10-16 23:05:09 +0300

    page done

---
+++ src/js/scripts.js
@@ -0,0 +1,40 @@
+window.onload = function() {
+  useWhiteColor();
+}
+
+
+
+
+function useWhiteColor() {
+  document.getElementById('tshirt-white').classList.remove('none');
+  document.getElementById('tshirt-green').classList.add('none');
+  document.getElementById('tshirt-yellow').classList.add('none');
+
+  document.getElementById('tshirt-white-variable-button').classList.add('variant_active');
+  document.getElementById('tshirt-green-variable-button').classList.remove('variant_active');
+  document.getElementById('tshirt-yellow-variable-button').classList.remove('variant_active');
+}
+
+
+
+function useGreenColor() {
+  document.getElementById('tshirt-white').classList.add('none');
+  document.getElementById('tshirt-green').classList.remove('none');
+  document.getElementById('tshirt-yellow').classList.add('none');
+
+  document.getElementById('tshirt-white-variable-button').classList.remove('variant_active');
+  document.getElementById('tshirt-green-variable-button').classList.add('variant_active');
+  document.getElementById('tshirt-yellow-variable-button').classList.remove('variant_active');
+}
+
+
+
+function useYellowColor() {
+  document.getElementById('tshirt-white').classList.add('none');
+  document.getElementById('tshirt-green').classList.add('none');
+  document.getElementById('tshirt-yellow').classList.remove('none');
+
+  document.getElementById('tshirt-white-variable-button').classList.remove('variant_active');
+  document.getElementById('tshirt-green-variable-button').classList.remove('variant_active');
+  document.getElementById('tshirt-yellow-variable-button').classList.add('variant_active');
+}
