d480dfdc7d99210ed23f86ab8034dbb0880ed6bc
Oksana <merkulova.0032@gmail.com>
2017-10-28 22:42:13 +0300

    gulp sourcemaps add

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const browserSync = require('browser-sync');
+const gulp         = require('gulp');
+const less         = require('gulp-less');
+const browserSync  = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
 const rename       = require('gulp-rename');
 const ejs          = require('gulp-ejs');
 const gutil        = require('gulp-util');
+const sourcemaps   = require('gulp-sourcemaps');
 
 // Автоперезагрузка при изменении файлов в папке `dist`:
 // Принцип: меняем файлы в `/src`, они обрабатываются и переносятся в `dist` и срабатывает автоперезагрузка.
@@ -22,10 +23,23 @@ gulp.task('livereload', () => {
     });
 });
 
+
+
+ gulp.task('scripts-dist', function() {
+   gulp.src('js/**/*.js')
+    // .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('styles', () => {
     gulp.src('src/less/main.less')
+		    // .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(autoprefixer())
+				// .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'));
 });
 
